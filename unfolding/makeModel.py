ad296a99f5ce5380232941795388e01ed49ebde0
Andrea Carlo Marini <andrea.marini@cern.ch>
2017-11-14 16:15:58 +0100

    more bug fix

--- unfolding/makeModel.py
+++ unfolding/makeModel.py
@@ -14,9 +14,9 @@ class Model():
         self.type="c&c" ## pdf
         self.w = None
         ## truth bins
-        self.nbins_x = 10
+        self.nbins_x = 5
         ## reco bins
-        self.nbins_y = 10
+        self.nbins_y = 5 ## if different, TODO: likely not work the diagonal--off-diagonal matching, check for i==j
         ## ntruth events
         self.nevents=1000
         self.bkg=False ## non resonant bkg
@@ -28,6 +28,11 @@ class Model():
         ###output file name
         self.fname = "workspace.root"
         self.dname = "datacard.txt"
+
+        #Internal
+
+    def __del__(self):
+        return
 
     def info(self):
         print "-------------------------"
@@ -119,10 +124,13 @@ class Model():
                 self.M.SetBinContent(binX,binY,c * x) 
 
     def Import(self,obj):
-        getattr(self.w,'import')(obj,ROOT.RooCmdArg())
+        print "* importing",obj.GetName()
+        #getattr(self.w,'import')(obj,ROOT.RooCmdArg())
+        getattr(self.w,'import')(obj,ROOT.RooCmdArg(ROOT.RooFit.RecycleConflictNodes()))
 
     def doWorkspace(self):
         self.w = ROOT.RooWorkspace("w","w")
+        ROOT.SetOwnership(self.w,False)
         ##self.pdf = ROOT.TF1("myfunc","x*x*x*TMath::Exp(-x)/5.9380",0,10)
         #create observable x (truth)
         print "-> construct real or fake pdfs for each entry in the matrix" 
@@ -131,7 +139,20 @@ class Model():
 
         self.Import(self.mgg)
         self.Import(self.MH )
+
+        ## construct data
+        #self.dataObs=[]
+        for j in range(0,self.nbins_y):
+            nbins=200 ## every 0.2 GeV
+            if self.type=='c&c':nbins=1
+            h=ROOT.TH1D("h_RecoBin%d"%j,"Reco empty histogram",nbins,110,150) # in mgg
+            dh = ROOT.RooDataHist("dataObs_RecoBin%d"%j,"Data Hist",ROOT.RooArgList(self.mgg),h.Clone())
+            self.Import(dh)
+            #self.dataObs.extend([h,dh])
+            #ROOT.SetOwnership(h,False)
+            #ROOT.SetOwnership(dh,False)
         
+        ## construct pdfs
         if self.type =='pdf':
             self.sigmaD = ROOT.RooRealVar("sigmaD","sigma diagonal elements",1.)
             self.sigmaO = ROOT.RooRealVar("sigmaO","sigma off-diagonal elements",2.)
@@ -147,8 +168,9 @@ class Model():
                 binY= self.Mhat.GetYaxis().FindBin(j+0.5)
 
                 norm = ROOT.RooRealVar(name +"_norm","normalization of xxx",self.M.GetBinContent(binX,binY))
+                norm.setConstant()
                 if self.type == 'c&c':
-                    pdf = ROOT.RooUniform(name,"Fake pdf for "+name,self.mgg)
+                    pdf = ROOT.RooUniform(name,"Fake pdf for "+name,ROOT.RooArgSet(self.mgg))
                 elif self.type == 'pdf':
                     if i==j:pdf = ROOT.RooGaussian(name,"Gauss pdf for "+name,self.mgg,self.MH,self.sigmaD)
                     else:pdf = ROOT.RooGaussian(name,"Gauss pdf for "+name,self.mgg,self.MH,self.sigmaO)
@@ -164,9 +186,10 @@ class Model():
                 name = "Bkg_RecoBin%d"%(j)
                 binY= self.Mhat.GetYaxis().FindBin(j+0.5)
                 norm = ROOT.RooRealVar(name +"_norm","normalization of xxx",self.b_th1.GetBinContent(binY))
+                norm.setConstant()
                 
                 if self.type=='c&c':
-                    pdf = ROOT.RooUniform(name,"Fake pdf for "+name,self.mgg)
+                    pdf = ROOT.RooUniform(name,"Fake pdf for "+name,ROOT.RooArgSet(self.mgg))
                 elif self.type == 'pdf':
                     pdf = ROOT.RooExponential(name,"Exp pdf for "+name,self.mgg,self.tau)
 
@@ -201,10 +224,11 @@ class Model():
         #    for j in range(0,self.nbins_y):
         #name = "GenBin%d_RecoBin%d"%(i,j)
         #name = "Bkg_RecoBin%d"%(j)
-        self.datacard.write("shape * * "+self.fname+" w:$PROCESS_$CHANNEL\n")
+        self.datacard.write("shapes data_obs * "+self.fname+" w:dataObs_$CHANNEL\n")
+        self.datacard.write("shapes * * "+self.fname+" w:$PROCESS_$CHANNEL\n")
         self.datacard.write("------------------------\n")
-        self.datacard.write("bin" + '\t'.join( ['RecoBin%d'%j for j in range(0,self.nbins_y) ] ) + '\n')
-        self.datacard.write("observation" + '\t'.join( ['-1' for j in range(0,self.nbins_y) ] ) + '\n')
+        self.datacard.write("bin\t" + '\t'.join( ['RecoBin%d'%j for j in range(0,self.nbins_y) ] ) + '\n')
+        self.datacard.write("observation\t" + '\t'.join( ['-1' for j in range(0,self.nbins_y) ] ) + '\n')
         ## observation
         self.datacard.write("------------------------\n")
         binline = "bin"
@@ -216,7 +240,7 @@ class Model():
             for j in range(0,self.nbins_y):
                 binline +="\t"+'RecoBin%d'%j 
                 procline += "\t"+'GenBin%d'%i
-                procline2 += "\t"+'-%d'%i
+                procline2 += "\t"+'%d'%(-i)
                 rateline += "\t" +"1"
 
         if self.bkg:
@@ -232,19 +256,41 @@ class Model():
         ## systematics
 
         ## write regularization lines
+        self.datacard.write("#######################################\n")
+        self.datacard.write("###### Regularization lines. Uncomment to enable regularization.\n")
+        self.datacard.write("###### Requsites for regularization studies:\n")
+        self.datacard.write("######  git pull git@github.com:amarini/HiggsAnalysis-CombinedLimit/topic_regularization2016\n")
+        self.datacard.write("## delta extArg 0.03\n") 
+        self.datacard.write("## nuisance edit delta freeze\n")
+        for i in range(0,self.nbins_x):
+            if i==0 or i==self.nbins_x: continue
+            self.datacard.write("## constr%d"%i+ " constr const%(bin)d_In[0.],RooFormulaVar::fconstr%(bin)d(\"r_Bin%(prev)d+r_Bin%(next)d-2*r_Bin%(bin)d\",{r_Bin%(prev)d,r_Bin%(bin)d,r_Bin%(next)d}),constr%(bin)d_S[delta]\n" %{"bin":i,"next":i+1,"prev":i-1} )
+        self.datacard.write("#######################################\n")
 
-        ## write commands
-        rootname = re.sub('.txt','.root',self.dname)
         self.datacard.write("\n")
+        self.datacard.write("\n")
+        self.datacard.write("\n")
+        ## write commands
+
+        #for i in range(nbinsG):
+        #   cmd+= " --PO map='.*Bin%d.*':r_Bin%d[1,-1,20]"%(i,i)
+        #   cmd+= " --PO map='.*Bkg.*':r_Bkg[1]"
+        rootname = re.sub('.txt','.root',self.dname)
         self.datacard.write("###############################\n")
-        self.datacard.write("## Requsites for regularization studies:\n")
-        self.datacard.write("##  git pull git@github.com:amarini/HiggsAnalysis-CombinedLimit/topic_regularization2016\n")
         self.datacard.write("##                             \n")
         self.datacard.write("## Commands:\n")
-        self.datacard.write("## text2workspace.py -X-allow-no-background -o "+rootname+" "+self.dname+"\n")
-        self.datacard.write("##   -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO map='.*Bin%d.*:r_Bin%d[1,-1,20]'\n")
-        self.datacard.write("## combine -M MultiDimFit "+rootname+"\n")
-        self.datacard.write("## combine -M MultiDimFit --algo=grid --points=100 -P GenBin1 --floatOtherPOIs=1 "+rootname+"\n")
+        self.datacard.write("## text2workspace.py -m 125 --X-allow-no-background -o "+rootname+" "+self.dname+"\n")
+        pois_map = ' '.join(["--PO map='.*GenBin%d.*:r_Bin%d[1,-1,20]'"%(i,i) for i in range(0,self.nbins_x)])
+        self.datacard.write("##   -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel "+pois_map+"\n")
+        self.datacard.write("##\n")
+        binbybin = ' '.join(["--PO map='.*RecoBin%d.*:r_Bin%d[1,-1,20]'"%(i,i) for i in range(0,self.nbins_y)])
+        self.datacard.write("## for bin by bin use:\n")
+        self.datacard.write("##   -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel "+binbybin+"\n")
+        self.datacard.write("##\n")
+
+        expect="--setPhysicsModelParameters="+','.join(["r_Bin%d=1"%i for i in range(0,self.nbins_x)])
+        self.datacard.write("## combine -M MultiDimFit "+expect+" -t -1 -m 125 "+rootname+"\n")
+        self.datacard.write("## combine -M MultiDimFit "+expect+" -t -1 -m 125 --algo=grid --points=100 -P r_Bin1 --floatOtherPOIs=1 "+rootname+"\n")
         self.datacard.write("###############################\n")
         self.datacard.close()
 
@@ -252,7 +298,12 @@ class Model():
 ##        MAIN        ##
 ########################
 if __name__=="__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    opts,args = parser.parse_args()
+
     model=Model()
     model.type='pdf'
+    #model.type='c&c'
     model.run()
     print "---- DONE ----"
